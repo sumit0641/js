e9ced668331d93e57ab225cf4aad79d452dcb3d3
sumit0641 <sumitkumaryadav1810@gmail.com>
2026-04-05 12:01:15 +0000

    numbers and maths in js

---
+++ basics_01/nums_and_math06.js
@@ -0,0 +1,42 @@
+const score = 400
+
+const balance = new Number(100)
+console.log(balance);
+
+
+console.log(balance.toString());
+console.log(balance.toFixed());
+const otherNumber = 123.9867
+
+console.log(otherNumber.toPrecision());
+
+
+const hundreds = 1000000
+console.log
+(hundreds.toLocalStrings('en-IN'));
+// ++++++++++++++++maths++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4)); // 4
+// console.log(Math.round(4.6));// 5
+// console.log(Math.ceil(4.2));// 5
+// console.log(Maths.floor(4.3));//4
+
+console.log(Math.random());// value will be bwt 0-1
+console.log(Math.random( )*10); // value bwt 0-10
+console.log((Math.random()*10)+1); // value bwt 1-10
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+
+
+
+
+
+
+
+
+
